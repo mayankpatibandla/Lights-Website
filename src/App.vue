be8453dcc6b612703d739ba09d419ac9dd4cfe08
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-09-03 23:37:25 -0400

    Reset to a blank vue project

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,27 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <!--Login-->
-  <div data-netlify-identity-button></div>
-
-  <div v-if="isLoggedIn">
-    <p>Secret Content</p>
-  </div>
-  <div v-else>
-    <p>Not logged in!!</p>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      isLoggedIn: false
-    }
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Options({
+  components: {
+    HelloWorld,
   },
-  async created() {
-    // https://www.youtube.com/watch?v=uY1HTcTiUkM&list=PLwpjN-4DtVRZfN1GgXCkCNVC9V711zKfg&index=4
-    // 11:41
-    netlifyIdentity.on('init', async (user) => {
-      if (user) {
-        this.isLoggedIn = true
-      }
-    })
-  }
-}
+})
+export default class App extends Vue {}
 </script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
