fc8638e9c93c127e123da6020697a98a62f73287
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-09-04 00:39:01 -0400

    Login works

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <div class="header">
+      <div data-netlify-identity-menu></div>
+    </div>
+    <div v-if="isLoggedIn">
+      <div><a href="https://google.com">ISLOGGEDIN!!!</a></div>
+    </div>
+    <div v-else><h1>NOT LOGGED IN!!!</h1></div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Options({
-  components: {
-    HelloWorld,
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'App',
+  data() {
+    return {
+      isLoggedIn: false,
+      token: ''
+    }
   },
+  async created() {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const self = this
+
+    // eslint-disable-next-line no-undef, @typescript-eslint/no-explicit-any
+    netlifyIdentity.on('init', async (user: any) => {
+      if (user) {
+        self.token = user.token.access_token
+        self.isLoggedIn = true
+      }
+    })
+
+    // eslint-disable-next-line no-undef
+    netlifyIdentity.on('login', (user: { token: { access_token: string } }) => {
+      self.token = user.token.access_token
+      self.isLoggedIn = true
+    })
+
+    // eslint-disable-next-line no-undef
+    netlifyIdentity.on('logout', () => {
+      self.token = ''
+      self.isLoggedIn = false
+    })
+  }
 })
-export default class App extends Vue {}
 </script>
 
 <style lang="scss">
